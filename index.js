9ed5e5591966254b663be614db5f4cc2bb7c4108
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-06-01 14:53:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(people, occasion) {
+    let output =[]
+    for (let i = 0; i < people.length; i++) {
+        output.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return output
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
