c2cb9cc6d648331b3987fa94a28fdfbca21db7e8
klarissasmith <ksmith_email@yahoo.com>
2020-06-03 21:53:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown() {
+    let countdown = 10
+    while (countdown >= 0)
+    console.log(countdown--);
+}
+
+function writeCards(array, string) {
+    let newarray = []
+    for (let i = 0; i < array.length; i++){
+        
+            newarray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+    }
+    return newarray
+}
+
